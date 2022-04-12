efb85614ad03e3cca06d6632802efbadc9f2a71d
linxOD <linxOD@users.noreply.github.com>
2022-04-12 13:40:28 +0000

    deploy: e61e86a70e5d678c07ff10174b2cae6f03a6ad19

--- js/citation-date.js
+++ js/citation-date.js
@@ -1,15 +1,33 @@
 $(function() {
+
+    `
+    replacing citation date of app build with current date
+    in de and en format.
+
+    `
+
     $('.citationDateDe').each(function() {
         let date = new Date();
-        let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        $(this).html(date.toLocaleDateString('de-DE', options));
+        let options = { 
+            weekday: 'long', 
+            year: 'numeric',
+            month: 'long', 
+            day: 'numeric'
+        };
+        $(this).html(
+            date.toLocaleDateString('de-DE', options)
+        );
     });
-});
-
-$(function() {
     $('.citationDateEn').each(function() {
         let date = new Date();
-        let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        $(this).html(date.toLocaleDateString('en-EN', options));
+        let options = { 
+            weekday: 'long', 
+            year: 'numeric',
+            month: 'long', 
+            day: 'numeric'
+        };
+        $(this).html(
+            date.toLocaleDateString('en-EN', options)
+        );
     });
 });
