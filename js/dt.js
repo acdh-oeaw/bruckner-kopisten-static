65866753cd5327aeba3389010d140d69f80b0531
gucl-mu <gucl-mu@users.noreply.github.com>
2022-06-29 16:05:30 +0000

    deploy: cb4a348c4d2b295e5b0cc203559c5a6c61823a78

--- js/dt.js
+++ js/dt.js
@@ -24,6 +24,12 @@ function createDataTable(containerElement, pageLength) {
         pl = 50;
     }
     var table = $(`#${containerElement}`).DataTable({
+    
+        language: {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/German.json"
+        },
+    
+    
         dom: "'<'row controlwrapper'<'col-sm-4'f><'col-sm-4'i><'col-sm-4 exportbuttons'Br>>'" +
             "'<'row'<'col-sm-12't>>'" +
             "'<'row'<'col-sm-6 offset-sm-6'p>>'",
