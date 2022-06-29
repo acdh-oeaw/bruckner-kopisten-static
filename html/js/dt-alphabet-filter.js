5f5b322d6abdd9d1c6a7c01e724ca61911b2cf1c
cgubsch <clemens.gubsch@oeaw.ac.at>
2022-06-29 18:07:21 +0200

    test language

--- html/js/dt-alphabet-filter.js
+++ html/js/dt-alphabet-filter.js
@@ -33,6 +33,10 @@ function bin ( data ) {
 function createDataTable( tableID, pageLength, start ) {
     var table = $(  '#' + tableID ).DataTable(
         {
+            language: {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/German.json"
+        },
+        
             dom: "'<'row controlwrapper'<'col-sm-4'f><'col-sm-4'i><'col-sm-4 exportbuttons'Br>>'" +
                 "'<'row'<'col-sm-12't>>'" +
                 "'<'row'<'col-sm-6 offset-sm-6'p>>'",
